a14d08b712cdfe0948cfb971b20b38b738ea69f9
Muathasim Parambat <muth4muathasim@gmail.com>
2020-04-20 14:07:04 +0530

    Changed std::function to template parameter in EventDispatcher + Bind index buffer to GL_ARRAY_BUFFER the first time

--- Hazel/src/Hazel/Events/Event.h
+++ Hazel/src/Hazel/Events/Event.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <functional>
-
 #include "Hazel/Core.h"
 
 namespace Hazel 
@@ -50,18 +48,15 @@ public:
 
 class HAZEL_API EventDispatcher 
 {
-	template<typename T>
-	using EventFn = std::function<bool(T&)>;
-
 public:
 	EventDispatcher(Event& event) : m_Event(event) {}
 
-	template<typename T>
-	bool Dispatch(EventFn<T> func) 
+	template<typename T, typename F>
+	bool Dispatch(const F& func) 
 	{
 		if (m_Event.GetEventType() == T::GetStaticType()) 
 		{
-			m_Event.Handled = func(*(T*)&m_Event);
+			m_Event.Handled = func(static_cast<T&>(m_Event));
 			return true;
 		}
 		return false;
